7e53c5833272eb38b2731b18965027593c5b760f
stefanR2LA <128801771+stefanR2LA@users.noreply.github.com>
2023-12-18 19:07:59 +0100

    Delete week8-js/lecture directory

--- week8-js/lecture/hello.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('form').addEventListener('submit', function(event){
-        let name = document.querySelector('#name').value;
-        alert('hello, ' + name);
-        event.preventDefault();
-    });
-});
